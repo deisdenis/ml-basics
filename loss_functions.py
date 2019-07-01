40916fb47e06f26dfc3e3f084ec2ed7e6d9821b2
Denis Deis <deisdenis@gmail.com>
2019-07-01 23:19:24 +0300

    loss functions added
    Signed-off-by: deisdenis <deisdenis@gmail.com>

---
+++ loss_functions.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def hinge_loss(z):
+    if z >= 1:
+        return 0
+    return 1 - z
+
+
+def squared_error_loss(z):
+    return z**2/2
+
+
+def empirical_risk(feature_vector, labels, theta, theta_0=0, loss_function=hinge_loss):
+    n = feature_vector.shape[0]
+    risk_sum = 0
+    for i in range(n):
+        loss_parameter = labels[i] - (np.dot(theta, feature_vector[i, :]) + theta_0)
+        risk_sum += loss_function(loss_parameter)
+    return risk_sum/n
+
+
