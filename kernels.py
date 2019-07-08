ae9a9926c17719a292da2b3242e9b7deb4a56612
deisdenis <deisdenis@gmail.com>
2019-07-08 17:20:16 +0300

    rbf_kernel added
    Signed-off-by: deisdenis <deisdenis@gmail.com>

--- kernels.py
+++ kernels.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def polynomial_kernel(X, Y, c, p):
     """
         Compute the polynomial kernel between two matrices X and Y::
@@ -16,3 +17,24 @@ def polynomial_kernel(X, Y, c, p):
             kernel_matrix - (n, m) Numpy array containing the kernel matrix
     """
     return (np.matmul(X, Y.T)+c)**p
+
+
+def rbf_kernel(X, Y, gamma):
+    """
+        Compute the Gaussian RBF kernel between two matrices X and Y::
+            K(x, y) = exp(-gamma ||x-y||^2)
+        for each pair of rows x in X and y in Y.
+
+        Args:
+            X - (n, d) NumPy array (n datapoints each with d features)
+            Y - (m, d) NumPy array (m datapoints each with d features)
+            gamma - the gamma parameter of gaussian function (scalar)
+
+        Returns:
+            kernel_matrix - (n, m) Numpy array containing the kernel matrix
+    """
+
+    X_norm = np.sum(X ** 2, axis=-1)
+    Y_norm = np.sum(Y ** 2, axis=-1)
+    kernel_matrix = np.exp(-gamma * (X_norm[:, None] + Y_norm[None, :] - 2*np.dot(X, Y.T)))
+    return kernel_matrix
