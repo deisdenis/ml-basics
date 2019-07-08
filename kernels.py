6da2f63bbb6a264ba91c1acd8a8bf85970c33048
deisdenis <deisdenis@gmail.com>
2019-07-08 16:01:14 +0300

    polynomial kernel reworked:     trade-off coefficient implemented     degree of the polynomial implemented
    Signed-off-by: deisdenis <deisdenis@gmail.com>

---
+++ kernels.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def polynomial_kernel(X, Y, c, p):
+    """
+        Compute the polynomial kernel between two matrices X and Y::
+            K(x, y) = (<x, y> + c)^p
+        for each pair of rows x in X and y in Y.
+
+        Args:
+            X - (n, d) NumPy array (n datapoints each with d features)
+            Y - (m, d) NumPy array (m datapoints each with d features)
+            c - a coefficient to trade off high-order and low-order terms (scalar)
+            p - the degree of the polynomial kernel
+
+        Returns:
+            kernel_matrix - (n, m) Numpy array containing the kernel matrix
+    """
+    return (np.matmul(X, Y.T)+c)**p
